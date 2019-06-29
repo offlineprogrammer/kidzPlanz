778dd73c600582c501ed5cfc8ac455dddc9fc1e8
OfflineProgrammer <mmalaka@gmail.com>
2019-06-28 21:17:08 -0700

    capacitor

--- src/app/add-kid/add-kid.page.ts
+++ src/app/add-kid/add-kid.page.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-kid.page.scss'],
 })
 export class AddKidPage implements OnInit {
+ 
 
   constructor() { }
 
   ngOnInit() {
   }
+  
+
+  
 
 }
--- src/app/services/kids.service.ts
+++ src/app/services/kids.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import {Kid} from '../interfaces/kid';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class KidsService {
 
+  public kids: Kid[] = [];
+
   constructor() { }
+
+  createKid(name): void {
+
+    // Create a unique id that is one larger than the current largest id
+    let id = Math.max(...this.kids.map(kid => parseInt(kid.id)), 0) + 1;
+
+    this.kids.push({
+      id: id.toString(),
+      name,
+      photo: ''
+    });
+
+    this.save();
+
+  }
+
+  async save(): Promise<void> {
+    const { Storage } = Plugins;
+    await Storage.set({
+      key: 'kids',
+      value: JSON.stringify(this.kids)
+    });
+
+  }
+
 }
