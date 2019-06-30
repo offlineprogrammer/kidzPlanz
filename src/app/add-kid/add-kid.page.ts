5adc7f9fe21410f00e33d17704ce8ad6ffda030f
OfflineProgrammer <mmalaka@gmail.com>
2019-06-29 21:20:36 -0700

    Forms

--- src/app/add-kid/add-kid.page.ts
+++ src/app/add-kid/add-kid.page.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import {KidsService} from '../services/kids.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  KidsService
+} from '../services/kids.service';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-kid',
@@ -7,19 +18,34 @@ import {KidsService} from '../services/kids.service';
   styleUrls: ['./add-kid.page.scss'],
 })
 export class AddKidPage implements OnInit {
- 
+
+  public submitAttempt = false;
+  public addKidForm: FormGroup;
 
   constructor(
-    private kidsService: KidsService
-  ) { }
+    private kidsService: KidsService,
+    public formBuilder: FormBuilder
+  ) {
 
-  ngOnInit() {
-   // this.kidsService.load();
+    this.addKidForm = formBuilder.group({
+      kidname: ['', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])]
+
+    });
+
   }
 
-  addKid(){
-    co
-    this.addKid(kidname);
+  ngOnInit() {
+    // this.kidsService.load();
+  }
+
+  addKid() {
+    this.submitAttempt = true;
+    if (this.addKidForm.valid){
+    console.log('success!')
+    console.log(this.addKidForm.value);
+    console.log(this.addKidForm.controls.kidname.value);
+    this.kidsService.createKid(this.addKidForm.controls.kidname.value);
+       }
   }
 
 
