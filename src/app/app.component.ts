755a318ab1f22661636581a5fe94d201aecd9655
OfflineProgrammer <mmalaka@gmail.com>
2019-08-12 21:15:49 -0700

    Tracking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AnalyticsService } from './services/analytics.service';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +13,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent {
   constructor(
+    private analyticsService: AnalyticsService,
+    public router: Router,
+    private title: Title,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
@@ -18,6 +24,22 @@ export class AppComponent {
   }
 
   initializeApp() {
+
+    this.analyticsService.startTrackerWithId('207136898');
+    this.router.events
+    .subscribe(event => {
+      //observe router and when it start navigation it will track the view
+      if (event instanceof NavigationStart) {
+        let title = this.title.getTitle();
+        //get title if it was sent on state
+        if (this.router.getCurrentNavigation().extras.state) {
+          title = this.router.getCurrentNavigation().extras.state.title;
+        }
+        //pass url and page title 
+        this.analyticsService.trackView(event.url, title);
+      }
+    });
+    
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
