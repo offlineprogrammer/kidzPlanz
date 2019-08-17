5c1afac6cc14360626d849ee8a7c9db40410fba9
OfflineProgrammer <mmalaka@gmail.com>
2019-08-17 11:05:02 -0700

    Location Error

--- src/app/add-kid/add-kid.page.spec.ts
+++ src/app/add-kid/add-kid.page.spec.ts
@@ -1,6 +1,8 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+
 
 import { AddKidPage } from './add-kid.page';
 
@@ -10,9 +12,10 @@ describe('AddKidPage', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule],
+      imports: [ReactiveFormsModule,RouterTestingModule],
       declarations: [ AddKidPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      
     })
     .compileComponents();
   }));
