5e8396f908e1ace1d0006fb43d9a66654ed24420
OfflineProgrammer <mmalaka@gmail.com>
2019-08-23 11:44:44 -0700

    AddKidPage UnitTest

--- src/app/add-kid/add-kid.page.spec.ts
+++ src/app/add-kid/add-kid.page.spec.ts
@@ -3,22 +3,34 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { IonicModule } from '@ionic/angular';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { KidsService } from '../services/kids.service';
+import { KidsServiceMock } from '../mocks';
+
 
 
 import { AddKidPage } from './add-kid.page';
 
-/* describe('AddKidPage', () => {
+describe('AddKidPage', () => {
   let component: AddKidPage;
-  let fixture: ComponentFixture<AddKidPage>;
+  let fixture: ComponentFixture < AddKidPage > ;
+  let de: DebugElement;
+  let el: HTMLElement;
 
-  beforeEach(async(() => {
+  beforeEach(async (() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule,RouterTestingModule,IonicModule],
-      declarations: [ AddKidPage ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        declarations: [AddKidPage],
+        imports: [ReactiveFormsModule,RouterTestingModule,IonicModule],
 
-    })
-    .compileComponents();
+        providers: [{
+            provide: KidsService,
+            useClass: KidsServiceMock
+          }
+        ],
+        schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      })
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -27,8 +39,26 @@ import { AddKidPage } from './add-kid.page';
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    fixture.destroy();
+    component = null;
+    de = null;
+    el = null;
+  });
+
   it('should create', () => {
+    expect(fixture).toBeTruthy();
     expect(component).toBeTruthy();
   });
+
+  it('displays a Form to add kid', () => {
+    fixture.detectChanges();
+    de = fixture.debugElement.query(By.css("form ion-item ion-input"));
+    el = de.nativeElement;
+    console.log(el);
+    expect(el.getAttribute('formcontrolname')).toContain('kidname');
+    
+  });
+  
 });
- */
+
